570580347bacb35743e0f5ab37b07106279791cd
Roberto <bobo@di.unipmn.it>
2023-08-08 15:22:24 +0200

    Fixed release requirements

--- version.php
+++ version.php
@@ -28,5 +28,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version = 2023050900;
 $plugin->release = '1.0';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->requires = 2022041900;         // Requires this Moodle version.
+$plugin->requires = 2022112800;         // Requires this Moodle version.
 $plugin->component = 'local_concorsi';  // Full name of the plugin (used for diagnostics).
